5e9d76b14de8c18940e6c5122cde7442573da44f
JStudy369 <84898871+JStudy369@users.noreply.github.com>
2024-10-25 16:46:53 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,8 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=mydataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'choose up to 5 ingredients:'
@@ -57,3 +58,5 @@ if ingredients_list:
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
 #fv_df = st.dataframe(data=fruityvice_response.json()), use_container_width=True)
+
+
